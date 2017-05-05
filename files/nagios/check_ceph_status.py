06c92a9d347660dd71fe37fc23ae65a3ecf20e30
Xav Paice <xav.paice@canonical.com>
2017-05-05 21:50:56 +1200

    Update ceph nagios plugin
    Changes ceph plugin so it ignores ceph rebalancing unless there is a
    large percentage of misplaced/degraded objects (return warning for that)
    
    Change-Id: I317448cd769597068a706d3944d9d5419e0445c1

--- files/nagios/check_ceph_status.py
+++ files/nagios/check_ceph_status.py
@@ -2,43 +2,87 @@
 
 # Copyright (C) 2014 Canonical
 # All Rights Reserved
-# Author: Jacek Nykis <jacek.nykis@canonical.com>
+# Authors: Jacek Nykis <jacek.nykis@canonical.com>
+#          Xav Paice <xav.paice@canonical.com>
 
 import re
 import argparse
+import json
 import subprocess
 import nagios_plugin
 
 
 def check_ceph_status(args):
+    regex = r'\d+ pgs (?:backfill_wait|backfilling|degraded|recovery_wait|' \
+            'stuck unclean)|recovery \d+\/\d+ objects (?:degraded|misplaced)'
+    status_critical = False
     if args.status_file:
         nagios_plugin.check_file_freshness(args.status_file, 3600)
-        with open(args.status_file, "r") as f:
-            lines = f.readlines()
-            status_data = dict(l.strip().split(' ', 1) for l in lines if len(l) > 1)
+        status_data = json.loads(open(args.status_file).read())
     else:
-        lines = subprocess.check_output(["ceph", "status"]).split('\n')
-        status_data = dict(l.strip().split(' ', 1) for l in lines if len(l) > 1)
+        try:
+            tree = subprocess.check_output(['ceph',
+                                            'status',
+                                            '--format json'])
+        except subprocess.CalledProcessError as e:
+            raise nagios_plugin.UnknownError(
+                "UNKNOWN: ceph status command failed with error: {}".format(e))
+        status_data = json.loads(tree)
 
-    if ('health' not in status_data
-            or 'monmap' not in status_data
-            or 'osdmap'not in status_data):
+    if ('health' not in status_data.keys() or
+            'monmap' not in status_data.keys() or
+            'pgmap' not in status_data.keys() or
+            'osdmap'not in status_data.keys()):
         raise nagios_plugin.UnknownError('UNKNOWN: status data is incomplete')
 
-    if status_data['health'] != 'HEALTH_OK':
-        msg = 'CRITICAL: ceph health status: "{}"'.format(status_data['health'])
-        raise nagios_plugin.CriticalError(msg)
-    osds = re.search("^.*: (\d+) osds: (\d+) up, (\d+) in", status_data['osdmap'])
-    if osds.group(1) > osds.group(2):  # not all OSDs are "up"
-        msg = 'CRITICAL: Some OSDs are not up. Total: {}, up: {}'.format(
-            osds.group(1), osds.group(2))
-        raise nagios_plugin.CriticalError(msg)
-    print "All OK"
+    if status_data['health']['overall_status'] != 'HEALTH_OK':
+        # Health is not OK, check if any lines are not in our list of OK
+        # any lines that don't match, check is critical
+        status_msg = []
+        for status in status_data['health']['summary']:
+            if not re.match(regex, status['summary']):
+                status_critical = True
+                status_msg.append(status['summary'])
+        # If we got this far, then the status is not OK but the status lines
+        # are all in our list of things we consider to be operational tasks.
+        # Check the thresholds and return OK if within boundary.
+        try:
+            if status_data['pgmap']['degraded_ratio'] > args.degraded_thresh:
+                status_critical = True
+                status_msg.append("Degraded ratio: {}%".format(
+                    status_data['pgmap']['degraded_ratio'] * 100))
+        except KeyError:
+            pass
+        try:
+            if status_data['pgmap']['misplaced_ratio'] > args.misplaced_thresh:
+                status_critical = True
+                status_msg.append("Misplaced ratio: {}%".format(
+                    status_data['pgmap']['misplaced_ratio'] * 100))
+        except KeyError:
+            pass
+        if status_critical:
+            msg = 'CRITICAL: ceph health: "{} {}"'.format(
+                  status_data['health']['overall_status'],
+                  ", ".join(status_msg))
+            raise nagios_plugin.CriticalError(msg)
+        if status_data['health']['overall_status'] == 'HEALTH_WARN':
+            msg = "WARNING: ceph misplaced {}%, degraded {}%".format(
+                  status_data['pgmap']['misplaced_ratio'] * 100,
+                  status_data['pgmap']['degraded_ratio'] * 100)
+            raise nagios_plugin.WarnError(msg)
+    print("All OK")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Check ceph status')
     parser.add_argument('-f', '--file', dest='status_file',
-                        default=False, help='Optional file with "ceph status" output')
+                        default=False,
+                        help='Optional file with "ceph status" output')
+    parser.add_argument('--degraded_thresh', dest='degraded_thresh',
+                        default=0.1,
+                        help="Threshold for degraded ratio (0.1 = 10%)")
+    parser.add_argument('--misplaced_thresh', dest='misplaced_thresh',
+                        default=0.1,
+                        help="Threshold for misplaced ratio (0.1 = 10%)")
     args = parser.parse_args()
     nagios_plugin.try_check(check_ceph_status, args)
